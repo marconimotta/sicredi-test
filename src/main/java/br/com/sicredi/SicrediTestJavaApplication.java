7e59beb0720edae5db7ced14face2bb418f050b7
Marconi Motta <marconi.motta@gok.digital>
2021-06-12 17:14:39 -0300

    clean version

---
+++ src/main/java/br/com/sicredi/SicrediTestJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediTestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediTestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sicredi/SicrediTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
