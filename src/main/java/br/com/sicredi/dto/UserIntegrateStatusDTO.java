355ca33f4e6cd5565dd8a8263e8f92218b896ef2
marconimotta <marconi.motta@hotmail.com>
2021-06-13 18:58:02 -0300

    [SIC-001]: Readme and change into UserIntegrateStatusDTO

--- src/main/java/br/com/sicredi/dto/UserIntegrateStatusDTO.java
+++ src/main/java/br/com/sicredi/dto/UserIntegrateStatusDTO.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(value = Include.NON_NULL)
 public class UserIntegrateStatusDTO {
 
